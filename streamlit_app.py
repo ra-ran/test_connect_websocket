43f9d0a434933c9d04e2f5fc2353b2113a691b33
ra-ran <93249249+ra-ran@users.noreply.github.com>
2024-08-21 15:11:40 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 from streamlit.web.server.websocket_headers import _get_websocket_headers
 
-headers = _get_websocket_headers()
-st.write(headers['Cookie'].split('=')[1])
+st.write(headers)
