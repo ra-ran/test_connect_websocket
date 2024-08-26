c5f4e44a9bac98fe22012aa0756a4071ed8929aa
ra-ran <93249249+ra-ran@users.noreply.github.com>
2024-08-26 13:16:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import socket
+from streamlit.web.server.websocket_headers import _get_websocket_headers
 
-def get_local_ip():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-        s.close()
-        return ip
-    except Exception:
-        return "No IP."
+headers = _get_websocket_headers()
+st.write(headers)
 
-st.title("IP 주소 확인")
-ip = get_local_ip()
-st.write(f"IP 주소: {ip}")
+# def get_local_ip():
+#     try:
+#         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#         s.connect(("8.8.8.8", 80))
+#         ip = s.getsockname()[0]
+#         s.close()
+#         return ip
+#     except Exception:
+#         return "No IP."
+
+# st.title("IP 주소 확인")
+# ip = get_local_ip()
+# st.write(f"IP 주소: {ip}")
