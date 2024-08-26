75fd5d5fbf737361beac30e8d5e59d3154a35bb3
ra-ran <93249249+ra-ran@users.noreply.github.com>
2024-08-26 13:48:10 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,9 @@
 import streamlit as st
 import socket
-from streamlit.web.server.websocket_headers import _get_websocket_headers
-import extra_streamlit_components as stx
+from streamlit.web.server.websocket_headers import _get_websocket_header
 
-cookies = stx.cookie_manager.get_all()
-cookies = stx.cookie_manager.get_all()
-st.write(cookies)
-
-def open(self):
-    # self.request.remote_ip contains the IP address of the user connecting
-    # to the Streamlit app. You could send this to an analytics platform like MixPanel
-    ip = self.request.remote_ip
-    self._session = self._server._create_report_session(self)
-    st.write(ip)
-
-# headers = _get_websocket_headers()
-# st.write(headers)
+headers = _get_websocket_headers()
+st.write(headers)
 
 # def get_local_ip():
 #     try:
@@ -29,6 +17,5 @@ def open(self):
 
 st.title("IP 주소 확인")
 # ip = get_local_ip()
-open()
 # st.write(ip)
 # st.write(f"IP 주소: {ip}")
